e7263f065339d2c08930e0fd7140ea93b831b3ad
Jame Ng <build@jameng.dev>
2022-12-10 22:50:08 +0800

    read csv file from usb

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Papa from "papaparse";
 
 const App = () => {
   return (
-    <div className="md:container mx-auto flex flex-col items-center justify-center h-screen bg-gray-200">
+    <div className="md:container mx-auto flex flex-col items-center justify-center h-screen bg-gray-200 space-y-2">
       <p className=" text-indigo-400 text-2xl">
         React + Tailwind + Typescript + Electron = ❤
       </p>
       <div>
-        <input type="text" id="testing" />
+        <input type="text" />
+      </div>
+      <div>
+        <input
+          type="file"
+          onChange={(event) => {
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+              const csv = e.target.result;
+              console.log("onload csv", csv);
+              const data = Papa.parse(csv);
+              console.log("onload data", data);
+            };
+            reader.readAsText(event.target.files[0]);
+            console.log("file", event);
+          }}
+        />
       </div>
     </div>
   );
